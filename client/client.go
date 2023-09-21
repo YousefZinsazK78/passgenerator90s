c4af98dd9edab214fbb949343b821d66e781e9c9
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-09-21 13:31:23 +0330

    Add context to handler and use it when user disconnected...

--- client/client.go
+++ client/client.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 	log.SetOutput(os.Stdout)
+	ctx := context.Background()
+
 	req, err := http.NewRequest("GET", "http://localhost:8000/", nil)
 	if err != nil {
 		panic(err)
 	}
+	cancelCtx, cancel := context.WithTimeout(ctx, 50*time.Millisecond)
+	defer cancel()
+
+	req = req.WithContext(cancelCtx)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,13 +9,45 @@ import (
 	"time"
 )
 
+func doWork(ctx context.Context, resChan chan int) {
+	log.Println("[doWork] launch the doWork")
+	sum := 0
+	for {
+		log.Println("[doWork] one iteration")
+		time.Sleep(time.Millisecond)
+		select {
+		case <-ctx.Done():
+			log.Println("[doWork] ctx Done is received inside doWork")
+			return
+		default:
+			sum++
+			if sum > 1000 {
+				log.Println("[doWork] sum has reached 1000")
+				resChan <- sum
+				return
+			}
+		}
+	}
+
+}
+
 func main() {
 	log.SetOutput(os.Stdout)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("request received...")
-		time.Sleep(time.Minute * 1)
-		fmt.Fprint(w, "custom response")
-		log.Println("response sent...")
+		log.Println("[handler] request received...")
+		rCtx := r.Context()
+		resChan := make(chan int)
+
+		go doWork(rCtx, resChan)
+		select {
+		case <-rCtx.Done():
+			log.Println("[handler] context canceled in main handler, client is disconnected")
+		case result := <-resChan:
+			log.Println("[handler] received 1000")
+			log.Println("[handler] send response")
+			fmt.Fprintf(w, "response %d", result)
+			return
+		}
 	})
 
 	err := http.ListenAndServe(":8000", nil)
